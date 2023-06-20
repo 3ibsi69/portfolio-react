0891bfb081e6826f2b2b28071646d2e5db2e1648
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-20 11:40:58 +0100

    starting

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,15 @@
+function NavBar(){
+    return(
+        <nav className="navbar">
+            <div className="nav-links">
+                <ul>
+                    <li><a href="#" className="hover-effect">Home</a></li>
+                    <li><a href="#"className="hover-effect">About Me</a></li>
+                    <li><a href="#"className="hover-effect">Projects</a></li>
+                    <li><a href="#" className="hover-effect">Contact Me</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default NavBar;

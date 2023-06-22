f40ad52b84f16bb042b870a21475b76215be3cbe
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-22 12:40:57 +0100

    navbar-resp

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,38 +1,60 @@
-function NavBar({ scrollToAbout, scrollToExperience,scrollToCards }) {
-    return (
-      <nav className="navbar">
-        <div className="nav-links">
-          <ul>
-            <li>
-              <a href="#" className="hover-effect">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover-effect" onClick={scrollToAbout}>
-                About Me
-              </a>
-            </li>
-            
-            <li>
-              <a href="#" className="hover-effect" onClick={scrollToExperience}>
-                Experience
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover-effect" onClick={scrollToCards}>
-                Projects
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="mobile">
-          <i className="fas fa-bars"></i>
-          <i className="fas fa-times"></i>
-        </div>
-      </nav>
-    );
-  }
-  
-  export default NavBar;
-  
+import React, { useState } from "react";
+
+function NavBar({ scrollToAbout, scrollToExperience, scrollToCards }) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className={`nav-links ${isMobileMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#" className="hover-effect" onClick={closeMobileMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover-effect" onClick={() => { scrollToAbout(); closeMobileMenu();}}>
+              About Me
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover-effect"
+              onClick={() => {
+                scrollToExperience();
+                closeMobileMenu();
+              }}
+            >
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="hover-effect"
+              onClick={() => {
+                scrollToCards();
+                closeMobileMenu();
+              }}
+            >
+              Projects
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div id="mobile" onClick={toggleMobileMenu}>
+        <i className={`fas fa-${isMobileMenuOpen ? "times" : "bars"}`}></i>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;

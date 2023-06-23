daa44bb29be0712ded8c1edf057c439a6df55f61
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-23 10:16:10 +0100

    fixed-deployment

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+//import a pdf
+import pdf from '../Pictures/Ahmed_Abbassi_CV_en.pdf';
 
 function Home() {
   const [currentText, setCurrentText] = useState('');
@@ -33,7 +35,7 @@ function Home() {
     <div>
       <h1 id="myname">abbassi ahmed</h1>
       <h3 id="text-move">{currentText}</h3>
-      <a href="/src/Pictures/Ahmed_Abbassi_CV_en.pdf" download>
+      <a href={pdf} download="Ahmed_Abbassi_CV_en.pdf">
         <button className="btn">Download Resume</button>
       </a>
     </div>
